3b0e77bb1bbd7afefadf8e4b644b3839e826c751
943785513@qq.com <943785513@qq.com>
2021-04-21 15:49:13 +0800

    update the map slice function etc.

--- src/day02/06slice/main.go
+++ src/day02/06slice/main.go
@@ -29,4 +29,27 @@ func main() {
 	fmt.Println(s4)
 	s5[1] = 200
 	fmt.Println(s4)
+
+	var s6 []int
+	// 自动初始化切片
+	s6 = append(s6, 1)
+	
+	// var s7 []int // 为nuil --[]
+	// var s7 = make([]int, 0, 3) // 由于此时长度为0 --[]
+							   // 然而copy函数不会扩容所以是[]
+	var s7 = make([]int, 3)    // 此时开辟了3个空间 -- [1 0 0]
+	copy(s7, s6)
+	fmt.Println(s7)
+
+	// 指针
+	// Go里面的指针只能读取不能修改，不能修改指针变量指向的地址
+	addr := "重庆邮电大学信科大厦"
+	addrP := &addr
+	fmt.Println(addrP)
+	fmt.Printf("%T\n", addrP)
+	// 0xc000088230
+	// *string
+	addrV := *addrP
+	fmt.Println()
+	fmt.Println(addrV)
 }
